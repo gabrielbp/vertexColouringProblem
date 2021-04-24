d4d40e57efb28236d584b0af5fec64398dc12511
Marcos Landi <marcoswinkellandi@gmail.com>
2021-04-24 14:38:34 -0300

    cria programa python e submete instancias

---
+++ vertexColoringProblem.py
@@ -0,0 +1,18 @@
+import networkx as nx
+
+G = nx.Graph()
+
+with open("2-FullIns_4.col", "r") as f:
+    
+    for i in range(6):
+        f.readline()
+    
+    infoGraph = f.readline()
+    n_vertices = int(infoGraph.split()[2])
+    n_edges = int(infoGraph.split()[3])
+    
+    for i in range(n_edges):
+        edgeString = f.readline()
+        G.add_edge(int(edgeString.split()[1]), int(edgeString.split()[2]))
+    
+    print(list(G[1].keys()))
