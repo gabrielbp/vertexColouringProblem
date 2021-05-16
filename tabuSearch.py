9e6d9bb897aeafa0c3319727f486c7c39ad23805
Marcos Landi <marcoswinkellandi@gmail.com>
2021-05-16 02:55:30 -0300

    mistura os métodos e ajusta parametros

--- tabuSearch.py
+++ tabuSearch.py
@@ -23,21 +23,21 @@ else:
 random.seed(a=seed)
 
 
-# def generateNeighbour(graph, coloring, tabuQueue):
-#     n_nodes = graph.number_of_nodes()
-#     random_node = random.randint(0, n_nodes-1)
+def generateNeighbour(graph, coloring, tabuQueue):
+    n_nodes = graph.number_of_nodes()
+    random_node = random.randint(0, n_nodes-1)
 
-#     color_space = set(range(len(coloring)))
-#     #color_space = set(coloring)
-#     color_space -= set([coloring[neighbour] for neighbour in list(G[random_node].keys())])
-#     color_space -= {coloring[random_node]}
-#     color_space -= set([x for x in tabuQueue if x[0] == random_node])
+    color_space = set(range(len(coloring)))
+    #color_space = set(coloring)
+    color_space -= set([coloring[neighbour] for neighbour in list(G[random_node].keys())])
+    color_space -= {coloring[random_node]}
+    color_space -= set([x for x in tabuQueue if x[0] == random_node])
 
-#     new_color = random.sample(color_space, 1)[0]
+    new_color = random.sample(color_space, 1)[0]
 
-#     new_coloring = copy.deepcopy(coloring)
-#     new_coloring[random_node] = new_color
-#     return (new_coloring, (random_node, new_color))
+    new_coloring = copy.deepcopy(coloring)
+    new_coloring[random_node] = new_color
+    return (new_coloring, (random_node, new_color))
 
 
 def generateNeighbourhood(graph, coloring, tabuQueue):
@@ -74,9 +74,11 @@ with open(instanceFileName, "r") as f:
 
 
 #maxIterationsWithoutImprovement = 50 * 20000 * 200 * n_nodes // (n_edges * 28)
-maxIterationsWithoutImprovement = 30000
+maxIterationsWithoutImprovement = 25000
+maxIterations = 5000 * 52 * 52 // (n_nodes + 20) ** 2
 numberOfNeighbours = 154*3
 tabuQueueSize = n_nodes*10
+maxNeighbourSize = 17000
 
 coloring = [i for i in range(n_nodes)]
 
@@ -89,37 +91,39 @@ currentSolutionValue = n_nodes
 
 timeStart = datetime.datetime.now()
 
-while iterationsWithoutImprovement < maxIterationsWithoutImprovement:
+#while iterationsWithoutImprovement < maxIterationsWithoutImprovement:
+while iteration < maxIterations:
     bestNeighbourValue = len(set(coloring))
     bestNeighbour = coloring
     bestNeighbourChangedNode = None
 
-    neighbourhood = generateNeighbourhood(G, coloring, tabuQueue)
+    if (len(set(coloring))*n_nodes <= maxNeighbourSize):
+        neighbourhood = generateNeighbourhood(G, coloring, tabuQueue)
+        bestNeighbours = []
+        for i in neighbourhood:
+            neighbourColoring = copy.deepcopy(coloring)
+            neighbourColoring[i[0]] = i[1]
+            changedNode = (i[0], coloring[i[0]])
+            currentSolutionValue = len(set(neighbourColoring))
 
-    bestNeighbours = []
-    for i in neighbourhood:
-        neighbourColoring = copy.deepcopy(coloring)
-        neighbourColoring[i[0]] = i[1]
-        changedNode = (i[0], coloring[i[0]])
-        currentSolutionValue = len(set(neighbourColoring))
-        if (currentSolutionValue <= bestNeighbourValue):
+            if (currentSolutionValue <= bestNeighbourValue):
+                if (currentSolutionValue < bestNeighbourValue):
+                    bestNeighbours = []
+                bestNeighbourValue = currentSolutionValue
+                bestNeighbours.append((bestNeighbourValue, neighbourColoring, bestNeighbourChangedNode))
+        
+        randomBestNeighbour = random.sample(bestNeighbours, 1)[0]
+        bestNeighbourValue = randomBestNeighbour[0]
+        bestNeighbour = randomBestNeighbour[1]
+        bestNeighbourChangedNode = randomBestNeighbour[2]
+    else:
+        for i in range(numberOfNeighbours):
+            neighbourColoring, changedNode = generateNeighbour(G, coloring, tabuQueue)
+            currentSolutionValue = len(set(neighbourColoring))
             if (currentSolutionValue < bestNeighbourValue):
-                bestNeighbours = []
-            bestNeighbourValue = currentSolutionValue
-            bestNeighbours.append((bestNeighbourValue, neighbourColoring, bestNeighbourChangedNode))
-    
-    randomBestNeighbour = random.sample(bestNeighbours, 1)[0]
-    bestNeighbourValue = randomBestNeighbour[0]
-    bestNeighbour = randomBestNeighbour[1]
-    bestNeighbourChangedNode = randomBestNeighbour[2]
-
-    # for i in range(numberOfNeighbours):
-    #     neighbourColoring, changedNode = generateNeighbour(G, coloring, tabuQueue)
-    #     currentSolutionValue = len(set(neighbourColoring))
-    #     if (currentSolutionValue < bestNeighbourValue):
-    #         bestNeighbourValue = currentSolutionValue
-    #         bestNeighbour = neighbourColoring
-    #         bestNeighbourChangedNode = changedNode
+                bestNeighbourValue = currentSolutionValue
+                bestNeighbour = neighbourColoring
+                bestNeighbourChangedNode = changedNode
     
     coloring = bestNeighbour
     currentSolutionValue = bestNeighbourValue
@@ -135,6 +139,7 @@ while iterationsWithoutImprovement < maxIterationsWithoutImprovement:
         tabuQueue.insert(0, bestNeighbourChangedNode)
 
     iteration += 1
+    print(iteration)
     iterationsWithoutImprovement += 1
 
 
