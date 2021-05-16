84237c4db6600833cb170e216f38ced246227d32
Marcos Landi <marcoswinkellandi@gmail.com>
2021-05-16 00:37:37 -0300

    geração alternativa de vizinhos

--- tabuSearch.py
+++ tabuSearch.py
@@ -6,6 +6,8 @@ import copy
 import datetime
 from math import floor
 
+from networkx.classes.function import neighbors
+
 testMode = len(sys.argv) == 2 and sys.argv[1] == "test"
 
 if (len(sys.argv) != 3 and not testMode):
@@ -21,21 +23,36 @@ else:
 random.seed(a=seed)
 
 
-def generateNeighbour(graph, coloring, tabuQueue):
+# def generateNeighbour(graph, coloring, tabuQueue):
+#     n_nodes = graph.number_of_nodes()
+#     random_node = random.randint(0, n_nodes-1)
+
+#     color_space = set(range(len(coloring)))
+#     #color_space = set(coloring)
+#     color_space -= set([coloring[neighbour] for neighbour in list(G[random_node].keys())])
+#     color_space -= {coloring[random_node]}
+#     color_space -= set([x for x in tabuQueue if x[0] == random_node])
+
+#     new_color = random.sample(color_space, 1)[0]
+
+#     new_coloring = copy.deepcopy(coloring)
+#     new_coloring[random_node] = new_color
+#     return (new_coloring, (random_node, new_color))
+
+
+def generateNeighbourhood(graph, coloring, tabuQueue):
     n_nodes = graph.number_of_nodes()
-    random_node = random.randint(0, n_nodes-1)
 
-    color_space = set(range(len(coloring)))
-    #color_space = set(coloring)
-    color_space -= set([coloring[neighbour] for neighbour in list(G[random_node].keys())])
-    color_space -= {coloring[random_node]}
-    color_space -= set([x for x in tabuQueue if x[0] == random_node])
+    color_space = set(coloring)
+    
+    neighbourhood = []
 
-    new_color = random.sample(color_space, 1)[0]
-
-    new_coloring = copy.deepcopy(coloring)
-    new_coloring[random_node] = new_color
-    return (new_coloring, (random_node, new_color))
+    for u in range(n_nodes):
+        for c in color_space:
+            if ((u, c) not in tabuQueue and c not in [coloring[neighbour] for neighbour in list(G[u].keys())]):
+                neighbourhood.append((u,c))
+    
+    return neighbourhood
 
 
 G = nx.Graph()
@@ -56,8 +73,9 @@ with open(instanceFileName, "r") as f:
         G.add_edge(int(edgeString.split()[1]) - 1, int(edgeString.split()[2]) - 1)
 
 
-maxIterationsWithoutImprovement = 50 * 20000 * 200 * n_nodes // (n_edges * 28)
-numberOfNeighbours = 50
+#maxIterationsWithoutImprovement = 50 * 20000 * 200 * n_nodes // (n_edges * 28)
+maxIterationsWithoutImprovement = 5000
+numberOfNeighbours = 154*3
 tabuQueueSize = n_nodes*10
 
 coloring = [i for i in range(n_nodes)]
@@ -76,13 +94,25 @@ while iterationsWithoutImprovement < maxIterationsWithoutImprovement:
     bestNeighbour = coloring
     bestNeighbourChangedNode = None
 
-    for i in range(numberOfNeighbours):
-        neighbourColoring, changedNode = generateNeighbour(G, coloring, tabuQueue)
+    neighbourhood = generateNeighbourhood(G, coloring, tabuQueue)
+
+    for i in neighbourhood:
+        neighbourColoring = copy.deepcopy(coloring)
+        neighbourColoring[i[0]] = i[1]
+        changedNode = (i[0], coloring[i[0]])
         currentSolutionValue = len(set(neighbourColoring))
         if (currentSolutionValue < bestNeighbourValue):
             bestNeighbourValue = currentSolutionValue
             bestNeighbour = neighbourColoring
             bestNeighbourChangedNode = changedNode
+
+    # for i in range(numberOfNeighbours):
+    #     neighbourColoring, changedNode = generateNeighbour(G, coloring, tabuQueue)
+    #     currentSolutionValue = len(set(neighbourColoring))
+    #     if (currentSolutionValue < bestNeighbourValue):
+    #         bestNeighbourValue = currentSolutionValue
+    #         bestNeighbour = neighbourColoring
+    #         bestNeighbourChangedNode = changedNode
     
     coloring = bestNeighbour
     currentSolutionValue = bestNeighbourValue
@@ -104,6 +134,8 @@ while iterationsWithoutImprovement < maxIterationsWithoutImprovement:
 seconds = (datetime.datetime.now() - timeStart) / datetime.timedelta(microseconds=1) / 1000000
 timeString = f'{floor(max(seconds,0))//60:02d}:{floor(max(seconds,0))%60:02d}'
 print(f'time since start: {timeString}')
+
+print(coloring)
 
 nx.draw(G, node_color=coloring, with_labels=True)
 plt.show()
